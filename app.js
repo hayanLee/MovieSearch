03020a5f3fefc0f20a798543633772629403c2b9
hayanLee <hayanlee.dev@gmail.com>
2024-04-23 15:00:15 +0900

    ✨ 검색 기능(대소문자 관계없이)

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ const $searchForm = document.querySelector('.searchForm');
 const $input = document.querySelector('.searchForm__input');
 const $btn = document.querySelector('.searchForm__btn');
 
-// $searchForm.addEventListener('submit', handleForm);
-// function handleForm(e) {
-//     e.preventDefault();
-//     let text = e.target[0].value;
-//     console.log(text);
-// }
+$searchForm.addEventListener('submit', handleForm);
+function handleForm(e) {
+    e.preventDefault();
+    let text = e.target[0].value;
+
+    if (text.trim() === '') return; // 빈 검색어 제거
+
+    const $cards = document.querySelectorAll('.card');
+    $cards.forEach((card) => {
+        let search = card.children[1].textContent.toLowerCase();
+        if (search.includes(text.toLowerCase())) card.style.display = 'block';
+        else card.style.display = 'none';
+    });
+}
 
 // MockData
 function getMovies() {
@@ -19,6 +27,14 @@ function getMovies() {
         })
         .catch((err) => console.error(err));
 }
+
+getMovies().then((movies) => {
+    // console.log(movies);
+    movies.forEach((movie) => {
+        const { backdrop_path: imgSrc, title, overview: content, id } = movie;
+        createMovieCard(imgSrc, title, content, id);
+    });
+});
 
 const createMovieCard = (imgSrc, title, content, id) => {
     const card = document.createElement('div');
@@ -52,11 +68,3 @@ const createMovieCard = (imgSrc, title, content, id) => {
 const clickCard = (e) => {
     alert(e.currentTarget.id);
 };
-
-getMovies().then((movies) => {
-    // console.log(movies);
-    movies.forEach((movie) => {
-        const { backdrop_path: imgSrc, title, overview: content, id } = movie;
-        createMovieCard(imgSrc, title, content, id);
-    });
-});
